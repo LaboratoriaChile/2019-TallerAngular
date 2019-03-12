c2f7653ec05ab13eed61a6ba10b515c2bec62f8d
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2019-03-12 15:28:38 -0300

    Comunicacion entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'Taller Angular 2019';
+  buttonProperties:any = {
+    color: 'green',
+    text: 'Soy un botón verde'
+  };
   
 
   handleClick(){
     this.title = 'Me han presionado el botón';
+  }
+
+  clickHandler(elStringQueRecibimos:string){
+    this.buttonProperties.text="Me han tocado el botón con "+elStringQueRecibimos;
   }
 }
--- src/app/special-button/special-button.component.ts
+++ src/app/special-button/special-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-special-button',
@@ -6,12 +6,18 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./special-button.component.css']
 })
 export class SpecialButtonComponent implements OnInit {
-  @Input() color:string;
+  @Input() buttonProperties:any;
   @Input() text:string;
+  @Output() clickHandlerEV:EventEmitter<string> = new EventEmitter();
   
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  handleClick(){
+    this.buttonProperties.color = 'red';
+    this.clickHandlerEV.emit("holi, soy un componente hijo");
+  }
 }
